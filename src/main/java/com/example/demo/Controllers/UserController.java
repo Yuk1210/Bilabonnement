e89a0b20b4ff2dbe14f1011a88723cae7bb347e8
SaddamSatoshiQassim <saddamqassimsq97@gmail.com>
2026-05-12 13:44:30 +0200

    tilføjede session til logind endpointet

--- src/main/java/com/example/demo/Controllers/UserController.java
+++ src/main/java/com/example/demo/Controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controllers;
 import com.example.demo.Models.User;
 import com.example.demo.Services.UserService;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +14,11 @@ public class UserController {
     private UserService userService = new UserService();
 
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) {
+    public String login(@RequestParam String username, @RequestParam String password, HttpSession session) {
         User user = userService.login(username, password);
 
         if (user != null) {
+            session.setAttribute("LoggedInUser",user);
             if (user.getRole().name().equals("DATAREGISTRERING")) {
                 return "rentalagreement";
             }
